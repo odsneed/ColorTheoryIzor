ce009e408c1cf3835f2ae4ff236a72cb258de245
Isaac Switzer <isaacnswitzer@students.abtech.edu>
2014-10-20 22:07:34 -0400

    lots of fixes!

--- HueMaker.js
+++ HueMaker.js
@@ -1,900 +1,223 @@
-$(document).ready(function() {
-	
-	$("#clear").on('click', function () {
+function clear() {
 		$(".color, .color1, .color2, .color3").remove();
+	}
+	
+function submit() {
+		
+	var color = $("#colorStart").val();
+	color = color.replace('#', '');
+
+	var red1 = color.charAt(0);
+	var red2 = color.charAt(1);
+	var green1 = color.charAt(2);
+	var green2 = color.charAt(3);
+	var blue1 = color.charAt(4);
+	var blue2 = color.charAt(5);
+	
+	function hexToDec1 (color) {
+		switch(color) {
+			case "0": dec1 = 0; break;
+			case "1": dec1 = 1*16; break;
+			case "2": dec1 = 2*16; break;
+			case "3": dec1 = 3*16; break;
+			case "4": dec1 = 4*16; break;
+			case "5": dec1 = 5*16; break;
+			case "6": dec1 = 6*16; break;
+			case "7": dec1 = 7*16; break;
+			case "8": dec1 = 8*16; break;
+			case "9": dec1 = 9*16; break;
+			case "a": dec1 = 10*16; break;
+			case "b": dec1 = 11*16; break;
+			case "c": dec1 = 12*16; break;
+			case "d": dec1 = 13*16; break;
+			case "e": dec1 = 14*16; break;
+			case "f": dec1 = 15*16; break;
+			default: alert ("incompatible number"); break;
+		}
+	}
+
+	function hexToDec2 (color) {
+		switch(color) {
+			case "0": dec2 = 0; break;
+			case "1": dec2 = 1; break;
+			case "2": dec2 = 2; break;
+			case "3": dec2 = 3; break;
+			case "4": dec2 = 4; break;
+			case "5": dec2 = 5; break;
+			case "6": dec2 = 6; break;
+			case "7": dec2 = 7; break;
+			case "8": dec2 = 8; break;
+			case "9": dec2 = 9; break;
+			case "a": dec2 = 10; break;
+			case "b": dec2 = 11; break;
+			case "c": dec2 = 12; break;
+			case "d": dec2 = 13; break;
+			case "e": dec2 = 14; break;
+			case "f": dec2 = 15; break;
+			default: alert ("incompatible number"); break;
+		}
+	}
+		
+	hexToDec1(red1);
+	var red1dec = dec1;
+			
+	hexToDec2(red2);
+	var red2dec = dec2;
+
+	hexToDec1(green1);
+	var green1dec = dec1;
+
+	hexToDec2(green2);
+	var green2dec = dec2;
+
+	hexToDec1(blue1);
+	var blue1dec = dec1;
+			
+	hexToDec2(blue2);
+	var blue2dec = dec2;
+
+	var red = red1dec + red2dec;
+	var green = green1dec + green2dec;
+	var blue = blue1dec + blue2dec;
+	var hueInterval = $("#hueInterval").val();
+	var eye = (Math.floor(-255/hueInterval)-1);
+	var aye = (Math.floor(255/hueInterval)+1);
+
+	function hueToHex1(hue) {
+		switch(hue) {
+			case 0: case 1: case 2:
+			case 3: case 4: case 5:
+			case 6: case 7: case 8:
+			case 9: hexHue1 = hue; break;
+			case 10: hexHue1 = "a"; break;
+			case 11: hexHue1 = "b"; break; 
+			case 12: hexHue1 = "c"; break;
+			case 13: hexHue1 = "d"; break;
+			case 14: hexHue1 = "e"; break;
+			case 15: hexHue1 = "f"; break;
+		}
+	}
+	
+	function hueToHex2(hue) {
+		switch(hue) {
+			case 0: case 1: case 2:
+			case 3: case 4: case 5:
+			case 6: case 7: case 8:
+			case 9: hexHue2 = hue; break;
+			case 10: hexHue2 = "a"; break;
+			case 11: hexHue2 = "b"; break; 
+			case 12: hexHue2 = "c"; break;
+			case 13: hexHue2 = "d"; break;
+			case 14: hexHue2 = "e"; break;
+			case 15: hexHue2 = "f"; break;
+		}
+	}
+		
+	for (var i=eye; i<aye; i++) {
+		var hueInterval = $("#hueInterval").val();
+		hueInterval = +hueInterval * i;
+
+		var redhex = (red + +hueInterval);
+
+		if (redhex>255)
+			{redhex = 255}
+		else{
+			if (redhex<0) {redhex = 0}
+		}
+		
+		var redhue1 = (Math.floor(redhex/16));
+		var redhue2 = (((redhex/16)% 1)*16);
+		
+		hueToHex1(redhue1);
+		var redhexhue1 = hexHue1;
+		
+		hueToHex2(redhue2);
+		var redhexhue2 = hexHue2;
+
+		var greenhex = (green + +hueInterval);
+		
+		if (greenhex>255)
+			{greenhex = 255}
+		else{
+			if (greenhex<0) {greenhex = 0}
+		}
+		
+		var greenhue1 = (Math.floor(greenhex/16));
+		var greenhue2 = (((greenhex/16)% 1)*16);
+		
+		hueToHex1(greenhue1);
+		var greenhexhue1 = hexHue1;
+		
+		hueToHex2(greenhue2);
+		var greenhexhue2 = hexHue2;
+
+		var bluehex = (blue + +hueInterval);
+		
+		if (bluehex>255)
+			{bluehex = 255}
+		else{
+			if (bluehex<0) {bluehex = 0}
+		}
+		
+		var bluehue1 = (Math.floor(bluehex/16));
+		var bluehue2 = (((bluehex/16)% 1)*16);
+		
+		hueToHex1(bluehue1);
+		var bluehexhue1 = hexHue1;
+		
+		hueToHex2(bluehue2);
+		var bluehexhue2 = hexHue2;
+
+		var color1 = (redhexhue1.toString() + redhexhue2.toString() + greenhexhue1.toString() + greenhexhue2.toString() + bluehexhue1.toString() + bluehexhue2.toString());
+
+		var hex = redhex + greenhex + bluehex;
+		
+		if (hex>0) {
+		if (hex<765) {
+		if (hex>300) {
+		if (redhex!=255) {
+		if (greenhex!=255) {
+		if (bluehex!=255) {			
+			$("<div class='color'></div>").css("background-color", "#" + color1).html("#" + color1 + "</br>R" + redhex + "</br>G" + greenhex +"</br>B" + bluehex).prependTo("#colors");}
+		else {
+			$("<div class='color2'></div>").css("background-color", "#" + color1).html("#" + color1 + "</br>R" + redhex + "</br>G" + greenhex +"</br>B" + bluehex).prependTo("#colors");}
+		}
+		else {
+		$("<div class='color2'></div>").css("background-color", "#" + color1).html("#" + color1 + "</br>R" + redhex + "</br>G" + greenhex +"</br>B" + bluehex).prependTo("#colors");}	
+		}
+		else {
+		$("<div class='color2'></div>").css("background-color", "#" + color1).html("#" + color1 + "</br>R" + redhex + "</br>G" + greenhex +"</br>B" + bluehex).prependTo("#colors");}
+		}
+		else {
+			if (redhex!=0) {
+		if (greenhex!=0) {
+		if (bluehex!=0) {	
+			$("<div class='color1'></div>").css("background-color", "#" + color1).html("#" + color1 + "</br>R" + redhex + "</br>G" + greenhex +"</br>B" + bluehex).prependTo("#colors");}
+		else {
+			$("<div class='color3'></div>").css("background-color", "#" + color1).html("#" + color1 + "</br>R" + redhex + "</br>G" + greenhex +"</br>B" + bluehex).prependTo("#colors");}	
+		}
+		else {
+			$("<div class='color3'></div>").css("background-color", "#" + color1).html("#" + color1 + "</br>R" + redhex + "</br>G" + greenhex +"</br>B" + bluehex).prependTo("#colors");}	
+		}	
+		else {
+			$("<div class='color3'></div>").css("background-color", "#" + color1).html("#" + color1 + "</br>R" + redhex + "</br>G" + greenhex +"</br>B" + bluehex).prependTo("#colors");}	
+		}
+		}
+		}
+}
+}
+	
+$(document).ready(function() {
+	$("#clear").click(function() {
+		clear();
+	});
+	
+	$("#submit2").on('click', function() {
+		submit();
 	});
 
-	$("#submit1").on('click', function () {
-		
-var color = $("#colorStart").val();
-color = color.replace('#', '');
-
-var red1 = color.charAt(0);
-var red2 = color.charAt(1);
-var green1 = color.charAt(2);
-var green2 = color.charAt(3);
-var blue1 = color.charAt(4);
-var blue2 = color.charAt(5);
-
-
-		
-
-if (red1 == "0") {
-var	red1dec = 0;
-
-}
-else {
-	if (red1 == "1") {
-		var	red1dec = 1*16;
-		
-	}
-	else {
-	
-	if (red1 == "2") {
-		var	red1dec = 2*16;
-		
-	}
-	else {
-	
-		if (red1 == "3") {
-		var	red1dec = 3*16;
-		
-	}
-	else {
-	
-		if (red1 == "4") {
-		var	red1dec = 4*16;
-		
-	}
-	else {
-	
-		if (red1 == "5") {
-		var	red1dec = 5*16;
-		
-	}
-	else {
-	
-		if (red1 == "6") {
-		var	red1dec = 6*16;
-		
-	}
-	else {
-	
-		if (red1 == "7") {
-		var	red1dec = 7*16;
-		
-	}
-	else {
-	
-		if (red1 == "8") {
-		var	red1dec = 8*16;
-		
-	}
-	else {
-	
-		if (red1 == "9") {
-		var	red1dec = 9*16;
-		
-	}
-	else {
-	
-		if (red1 == "a") {
-		var	red1dec = 10*16;
-		
-	}
-	else {
-	
-		if (red1 == "b") {
-		var	red1dec = 11*16;
-		
-	}
-	else {
-	
-		if (red1 == "c") {
-		var	red1dec = 12*16;
-		
-	}
-	else {
-	
-		if (red1 == "d") {
-		var	red1dec = 13*16;
-		
-	}
-	else {
-	
-		if (red1 == "e") {
-		var	red1dec = 14*16;
-		
-	}
-	else {
-	
-		if (red1 == "f") {
-		var	red1dec = 15*16;
-		
-	}
-	else {
-	
-		alert ("incompaible number");
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-
-
-if (red2 == "0") {
-var	red2dec = 0;
-
-}
-else {
-	if (red2 == "1") {
-		var	red2dec = 1;
-		
-	}
-	else {
-	
-	if (red2 == "2") {
-		var	red2dec = 2;
-		
-	}
-	else {
-	
-		if (red2 == "3") {
-		var	red2dec = 3;
-		
-	}
-	else {
-	
-		if (red2 == "4") {
-		var	red2dec = 4;
-		
-	}
-	else {
-	
-		if (red2 == "5") {
-		var	red2dec = 5;
-		
-	}
-	else {
-	
-		if (red2 == "6") {
-		var	red2dec = 6;
-		
-	}
-	else {
-	
-		if (red2 == "7") {
-		var	red2dec = 7;
-		
-	}
-	else {
-	
-		if (red2 == "8") {
-		var	red2dec = 8;
-		
-	}
-	else {
-	
-		if (red2 == "9") {
-		var	red2dec = 9;
-		
-	}
-	else {
-	
-		if (red2 == "a") {
-		var	red2dec = 10;
-		
-	}
-	else {
-	
-		if (red2 == "b") {
-		var	red2dec = 11;
-		
-	}
-	else {
-	
-		if (red2 == "c") {
-		var	red2dec = 12;
-		
-	}
-	else {
-	
-		if (red2 == "d") {
-		var	red2dec = 13;
-		
-	}
-	else {
-	
-		if (red2 == "e") {
-		var	red2dec = 14;
-		
-	}
-	else {
-	
-		if (red2 == "f") {
-		var	red2dec = 15;
-		
-	}
-	else {
-	
-		alert ("incompaible number");
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-
-
-if (green1 == "0") {
-var	green1dec = 0;
-
-}
-else {
-	if (green1 == "1") {
-		var	green1dec = 1*16;
-		
-	}
-	else {
-	
-	if (green1 == "2") {
-		var	green1dec = 2*16;
-		
-	}
-	else {
-	
-		if (green1 == "3") {
-		var	green1dec = 3*16;
-		
-	}
-	else {
-	
-		if (green1 == "4") {
-		var	green1dec = 4*16;
-		
-	}
-	else {
-	
-		if (green1 == "5") {
-		var	green1dec = 5*16;
-		
-	}
-	else {
-	
-		if (green1 == "6") {
-		var	green1dec = 6*16;
-		
-	}
-	else {
-	
-		if (green1 == "7") {
-		var	green1dec = 7*16;
-		
-	}
-	else {
-	
-		if (green1 == "8") {
-		var	green1dec = 8*16;
-		
-	}
-	else {
-	
-		if (green1 == "9") {
-		var	green1dec = 9*16;
-		
-	}
-	else {
-	
-		if (green1 == "a") {
-		var	green1dec = 10*16;
-		
-	}
-	else {
-	
-		if (green1 == "b") {
-		var	green1dec = 11*16;
-		
-	}
-	else {
-	
-		if (green1 == "c") {
-		var	green1dec = 12*16;
-		
-	}
-	else {
-	
-		if (green1 == "d") {
-		var	green1dec = 13*16;
-		
-	}
-	else {
-	
-		if (green1 == "e") {
-		var	green1dec = 14*16;
-		
-	}
-	else {
-	
-		if (green1 == "f") {
-		var	green1dec = 15*16;
-		
-	}
-	else {
-	
-		alert ("incompaible number");
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-
-
-if (green2 == "0") {
-var	green2dec = 0;
-
-}
-else {
-	if (green2 == "1") {
-		var	green2dec = 1;
-		
-	}
-	else {
-	
-	if (green2 == "2") {
-		var	green2dec = 2;
-		
-	}
-	else {
-	
-		if (green2 == "3") {
-		var	green2dec = 3;
-		
-	}
-	else {
-	
-		if (green2 == "4") {
-		var	green2dec = 4;
-		
-	}
-	else {
-	
-		if (green2 == "5") {
-		var	green2dec = 5;
-		
-	}
-	else {
-	
-		if (green2 == "6") {
-		var	green2dec = 6;
-		
-	}
-	else {
-	
-		if (green2 == "7") {
-		var	green2dec = 7;
-		
-	}
-	else {
-	
-		if (green2 == "8") {
-		var	green2dec = 8;
-		
-	}
-	else {
-	
-		if (green2 == "9") {
-		var	green2dec = 9;
-		
-	}
-	else {
-	
-		if (green2 == "a") {
-		var	green2dec = 10;
-		
-	}
-	else {
-	
-		if (green2 == "b") {
-		var	green2dec = 11;
-		
-	}
-	else {
-	
-		if (green2 == "c") {
-		var	green2dec = 12;
-		
-	}
-	else {
-	
-		if (green2 == "d") {
-		var	green2dec = 13;
-		
-	}
-	else {
-	
-		if (green2 == "e") {
-		var	green2dec = 14;
-		
-	}
-	else {
-	
-		if (green2 == "f") {
-		var	green2dec = 15;
-		
-	}
-	else {
-	
-		alert ("incompaible number");
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-
-if (blue1 == "0") {
-var	blue1dec = 0;
-
-}
-else {
-	if (blue1 == "1") {
-		var	blue1dec = 1*16;
-		
-	}
-	else {
-	
-	if (blue1 == "2") {
-		var	blue1dec = 2*16;
-		
-	}
-	else {
-	
-		if (blue1 == "3") {
-		var	blue1dec = 3*16;
-		
-	}
-	else {
-	
-		if (blue1 == "4") {
-		var	blue1dec = 4*16;
-		
-	}
-	else {
-	
-		if (blue1 == "5") {
-		var	blue1dec = 5*16;
-		
-	}
-	else {
-	
-		if (blue1 == "6") {
-		var	blue1dec = 6*16;
-		
-	}
-	else {
-	
-		if (blue1 == "7") {
-		var	blue1dec = 7*16;
-		
-	}
-	else {
-	
-		if (blue1 == "8") {
-		var	blue1dec = 8*16;
-		
-	}
-	else {
-	
-		if (blue1 == "9") {
-		var	blue1dec = 9*16;
-		
-	}
-	else {
-	
-		if (blue1 == "a") {
-		var	blue1dec = 10*16;
-		
-	}
-	else {
-	
-		if (blue1 == "b") {
-		var	blue1dec = 11*16;
-		
-	}
-	else {
-	
-		if (blue1 == "c") {
-		var	blue1dec = 12*16;
-		
-	}
-	else {
-	
-		if (blue1 == "d") {
-		var	blue1dec = 13*16;
-		
-	}
-	else {
-	
-		if (blue1 == "e") {
-		var	blue1dec = 14*16;
-		
-	}
-	else {
-	
-		if (blue1 == "f") {
-		var	blue1dec = 15*16;
-		
-	}
-	else {
-	
-		alert ("incompaible number");
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-
-
-if (blue2 == "0") {
-var	blue2dec = 0;
-
-}
-else {
-	if (blue2 == "1") {
-		var	blue2dec = 1;
-		
-	}
-	else {
-	
-	if (blue2 == "2") {
-		var	blue2dec = 2;
-		
-	}
-	else {
-	
-		if (blue2 == "3") {
-		var	blue2dec = 3;
-		
-	}
-	else {
-	
-		if (blue2 == "4") {
-		var	blue2dec = 4;
-		
-	}
-	else {
-	
-		if (blue2 == "5") {
-		var	blue2dec = 5;
-		
-	}
-	else {
-	
-		if (blue2 == "6") {
-		var	blue2dec = 6;
-		
-	}
-	else {
-	
-		if (blue2 == "7") {
-		var	blue2dec = 7;
-		
-	}
-	else {
-	
-		if (blue2 == "8") {
-		var	blue2dec = 8;
-		
-	}
-	else {
-	
-		if (blue2 == "9") {
-		var	blue2dec = 9;
-		
-	}
-	else {
-	
-		if (blue2 == "a") {
-		var	blue2dec = 10;
-		
-	}
-	else {
-	
-		if (blue2 == "b") {
-		var	blue2dec = 11;
-		
-	}
-	else {
-	
-		if (blue2 == "c") {
-		var	blue2dec = 12;
-		
-	}
-	else {
-	
-		if (blue2 == "d") {
-		var	blue2dec = 13;
-		
-	}
-	else {
-	
-		if (blue2 == "e") {
-		var	blue2dec = 14;
-		
-	}
-	else {
-	
-		if (blue2 == "f") {
-		var	blue2dec = 15;
-		
-	}
-	else {
-	
-		alert ("incompaible number");
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-//alert (red1dec);
-
-//alert = ("hueInterval");
-var red = red1dec + red2dec;
-var green = green1dec + green2dec;
-var blue = blue1dec + blue2dec;
-//if (blue>255) {blue = 255}
-//alert (blue);
-//alert ("red is " + red + " green is " + green + " blue is " + blue);
-//alert (red);
-
-	
-var hueInterval = $("#hueInterval").val();
-var eye = (Math.floor(-255/hueInterval)-1);
-var aye = (Math.floor(255/hueInterval)+1);
-//alert (eye);
-//alert (aye);
-for (var i=eye; i<aye; i++) {
-
-	
-//for (var i= (Math.ceiling(-255/hueInterval)); i< (Math.ceiling(255/hueInterval)); i++) {
-	
-var hueInterval = $("#hueInterval").val();
-hueInterval = +hueInterval * i;
-
-var redhex = (red + +hueInterval);
-
-if (redhex>255) {redhex = 255}
-else{
-if (redhex<0) {redhex = 0}
-}
-var redhue1 = (Math.floor(redhex/16));
-
-if (redhue1 == 10) { var redhexhue1 = "a" }
-else { if (redhue1 == 11) { var redhexhue1 = "b" } 
-else { if (redhue1 == 12) { var redhexhue1 = "c" } 
-else { if (redhue1 == 13) { var redhexhue1 = "d" } 
-else { if (redhue1 == 14) { var redhexhue1 = "e" } 
-else { if (redhue1 == 15) { var redhexhue1 = "f" } 
-else { redhexhue1 = redhue1 }
-}
-}
-}
-}
-}
-
-var redhue2 = (((redhex/16)% 1)*16);
-if (redhue2 == 10) { var redhexhue2 = "a" }
-else { if (redhue2 == 11) { var redhexhue2 = "b" } 
-else { if (redhue2 == 12) { var redhexhue2 = "c" } 
-else { if (redhue2 == 13) { var redhexhue2 = "d" } 
-else { if (redhue2 == 14) { var redhexhue2 = "e" } 
-else { if (redhue2 == 15) { var redhexhue2 = "f" } 
-else { redhexhue2 = redhue2 }
-}
-}
-}
-}
-}
-
-var greenhex = (green + +hueInterval);
-if (greenhex>255) {greenhex = 255}
-else{
-if (greenhex<0) {greenhex = 0}
-}
-var greenhue1 = (Math.floor(greenhex/16));
-if (greenhue1 == 10) { var greenhexhue1 = "a" }
-else { if (greenhue1 == 11) { var greenhexhue1 = "b" } 
-else { if (greenhue1 == 12) { var greenhexhue1 = "c" } 
-else { if (greenhue1 == 13) { var greenhexhue1 = "d" } 
-else { if (greenhue1 == 14) { var greenhexhue1 = "e" } 
-else { if (greenhue1 == 15) { var greenhexhue1 = "f" } 
-else { greenhexhue1 = greenhue1 }
-}
-}
-}
-}
-}
-
-var greenhue2 = (((greenhex/16)% 1)*16);
-if (greenhue2 == 10) { var greenhexhue2 = "a" }
-else { if (greenhue2 == 11) { var greenhexhue2 = "b" } 
-else { if (greenhue2 == 12) { var greenhexhue2 = "c" } 
-else { if (greenhue2 == 13) { var greenhexhue2 = "d" } 
-else { if (greenhue2 == 14) { var greenhexhue2 = "e" } 
-else { if (greenhue2 == 15) { var greenhexhue2 = "f" } 
-else { greenhexhue2 = greenhue2 }
-}
-}
-}
-}
-}
-
-var bluehex = (blue + +hueInterval);
-if (bluehex>255) {bluehex = 255}
-else{
-if (bluehex<0) {bluehex = 0}
-}
-var bluehue1 = (Math.floor(bluehex/16));
-if (bluehue1 == 10) { var bluehexhue1 = "a" }
-else { if (bluehue1 == 11) { var bluehexhue1 = "b" } 
-else { if (bluehue1 == 12) { var bluehexhue1 = "c" } 
-else { if (bluehue1 == 13) { var bluehexhue1 = "d" } 
-else { if (bluehue1 == 14) { var bluehexhue1 = "e" } 
-else { if (bluehue1 == 15) { var bluehexhue1 = "f" } 
-else { bluehexhue1 = bluehue1 }
-}
-}
-}
-}
-}
-
-var bluehue2 = (((bluehex/16)% 1)*16);
-if (bluehue2 == 10) { var bluehexhue2 = "a" }
-else { if (bluehue2 == 11) { var bluehexhue2 = "b" } 
-else { if (bluehue2 == 12) { var bluehexhue2 = "c" } 
-else { if (bluehue2 == 13) { var bluehexhue2 = "d" } 
-else { if (bluehue2 == 14) { var bluehexhue2 = "e" } 
-else { if (bluehue2 == 15) { var bluehexhue2 = "f" } 
-else { bluehexhue2 = bluehue2 }
-}
-}
-}
-}
-}
-
-//var redhex = (redhexhue1.toString() + redhexhue2.toString());
-//if (redhex>255) {redhex = 255}
-
-//var greenhex = (greenhexhue1.toString() + greenhexhue2.toString());
-//if (greenhex>255) {greenhex = 255}
-//var bluehex = (bluehexhue1.toString() + bluehexhue2.toString());
-//if (bluehex>255) {bluehex = 255}
-
-var color1 = (redhexhue1.toString() + redhexhue2.toString() + greenhexhue1.toString() + greenhexhue2.toString() + bluehexhue1.toString() + bluehexhue2.toString());
-//alert (color1);
-//var color1 = (redhex.toString() + greenhex.toString() + bluehex.toString());
-//if redhex < 256 { 
-//if greenhex < 256 { if bluehex < 256 {  if redhex > 0 { if greenhex > 0 { if bluehex > 0 
-
-var hex = redhex + greenhex + bluehex;
-if (hex>0) {
-if (hex<765) {
-if (hex>300) {
-if (redhex!=255) {
-if (greenhex!=255) {
-if (bluehex!=255) {			
-$("<div class='color'></div>").css("background-color", "#" + color1).html("#" + color1 + "</br>R" + redhex + "</br>G" + greenhex +"</br>B" + bluehex).prependTo("#colors");
-}
-else {
-$("<div class='color2'></div>").css("background-color", "#" + color1).html("#" + color1 + "</br>R" + redhex + "</br>G" + greenhex +"</br>B" + bluehex).prependTo("#colors");
-}
-}
-else {
-$("<div class='color2'></div>").css("background-color", "#" + color1).html("#" + color1 + "</br>R" + redhex + "</br>G" + greenhex +"</br>B" + bluehex).prependTo("#colors");
-}	
-}
-else {
-$("<div class='color2'></div>").css("background-color", "#" + color1).html("#" + color1 + "</br>R" + redhex + "</br>G" + greenhex +"</br>B" + bluehex).prependTo("#colors");
-}
-}
-else {
-	if (redhex!=0) {
-if (greenhex!=0) {
-if (bluehex!=0) {	
-	$("<div class='color1'></div>").css("background-color", "#" + color1).html("#" + color1 + "</br>R" + redhex + "</br>G" + greenhex +"</br>B" + bluehex).prependTo("#colors");
-}
-else {
-	$("<div class='color3'></div>").css("background-color", "#" + color1).html("#" + color1 + "</br>R" + redhex + "</br>G" + greenhex +"</br>B" + bluehex).prependTo("#colors");
-}	
-}
-else {
-	$("<div class='color3'></div>").css("background-color", "#" + color1).html("#" + color1 + "</br>R" + redhex + "</br>G" + greenhex +"</br>B" + bluehex).prependTo("#colors");
-}	
-}	
-else {
-	$("<div class='color3'></div>").css("background-color", "#" + color1).html("#" + color1 + "</br>R" + redhex + "</br>G" + greenhex +"</br>B" + bluehex).prependTo("#colors");
-}	
-	}
-
-}
-}
-//}
-//}}}}
-
-//$("#hue" + (i + 25)).css("background-color", "#" + color1).html("#" + color1 + "</br>R" + redhex + "</br>G" + greenhex +"</br>B" + bluehex);
- 
-//alert (color1);
-//var hue1 = (red.toString() + green.toString() + blue.toString());
-//alert (redhue1 + " and " + redhue2);
-//alert ("red1 is " + red1dec + " red2 is " + red2dec + " green1 is " + green1dec + " green2 is " + green2dec + " blue1 is " + blue1dec + " blue2 is " + blue2dec)
-} //end for loop		
-
-
-
-
-});
+	$("#submit1").on('click', function() {
+		clear();
+		submit();
+	});
 });
